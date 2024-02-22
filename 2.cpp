3938e454bd913d2c3900ceb30bae1ab857e89987
HasShahbazyan <138981687+hasmikshahbazyan2004@users.noreply.github.com>
2024-02-23 01:45:17 +0400

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int num1 = 10;
+	char sym = 'A';
+	float num2 = 2.2;
+	double num3 = 0.004;
+	bool flag = true;
+	
+	std::cout << num1 << std::endl;
+	std::cout << sym << std::endl;
+	std::cout << num2 << std::endl;
+	std::cout << num3 << std::endl;
+	std::cout << flag << std::endl;
+}
